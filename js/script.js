374f17f102dcfef4004fc1c1c3c364445a506a2b
Jexxme <82029475+jexxme@users.noreply.github.com>
2023-11-17 19:03:06 +0100

    a<dded reg

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ document.addEventListener('DOMContentLoaded', function() {
         { src: 'images/sortAlgorithms.png', title: 'sortAlgorithms.js', url: 'https://jexxme.github.io/sortAlgorithms' },
         { src: 'images/itVsRe.png', title: 'iterationVsRecursion.js', url: 'https://jexxme.github.io/rekursionVsIteration/' },
         { src: 'images/numberSystemGame.png', title: 'numberSystemGame.js', url: 'https://jexxme.github.io/numberSystemGameWeb/' },
-        
+        { src: 'images/multiStageRegister.png', title: 'multiStageRegister.js', url: 'https://jexxme.github.io/vdv-infos/' },
         // Add more images as needed with their corresponding URLs
     ];
 

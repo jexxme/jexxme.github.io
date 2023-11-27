45ca761957367ffe9b70520beba5e14ae97d19c4
Jexxme <82029475+jexxme@users.noreply.github.com>
2023-11-27 15:59:41 +0100

    updated for bpbQS

--- js/script.js
+++ js/script.js
@@ -1,10 +1,11 @@
 document.addEventListener('DOMContentLoaded', function() {
     const gallery = document.getElementById('gallery');
     const images = [
+        { src: 'images/bpbQS.png', title: 'backpackbattlesQS.js', url: 'https://jexxme.github.io/bpbQS/' },
         { src: 'images/sortAlgorithms.png', title: 'sortAlgorithms.js', url: 'https://jexxme.github.io/sortAlgorithms' },
         { src: 'images/itVsRe.png', title: 'iterationVsRecursion.js', url: 'https://jexxme.github.io/rekursionVsIteration/' },
-        { src: 'images/numberSystemGame.png', title: 'numberSystemGame.js', url: 'https://jexxme.github.io/numberSystemGameWeb/' },
         { src: 'images/multiStageRegister.png', title: 'multiStageRegister.js', url: 'https://jexxme.github.io/vdv-infos/' },
+        { src: 'images/numberSystemGame.png', title: 'numberSystemGame.js', url: 'https://jexxme.github.io/numberSystemGameWeb/' },
         // Add more images as needed with their corresponding URLs
     ];
 

eeac1ea6bdfe8557c2fc1f07298bc0137197981b
Jexxme <82029475+jexxme@users.noreply.github.com>
2023-11-28 22:35:31 +0100

    added Image2SVG

--- js/script.js
+++ js/script.js
@@ -6,6 +6,7 @@ document.addEventListener('DOMContentLoaded', function() {
         { src: 'images/itVsRe.png', title: 'iterationVsRecursion.js', url: 'https://jexxme.github.io/rekursionVsIteration/' },
         { src: 'images/multiStageRegister.png', title: 'multiStageRegister.js', url: 'https://jexxme.github.io/vdv-infos/' },
         { src: 'images/numberSystemGame.png', title: 'numberSystemGame.js', url: 'https://jexxme.github.io/numberSystemGameWeb/' },
+        { src: 'images/PNGtoSVG.png', title: 'PNGtoSVG.js', url: 'https://jexxme.github.io/vectorIT/' },
         // Add more images as needed with their corresponding URLs
     ];
 
